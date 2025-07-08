c225846aa5732796064ef658aa5fc973db851202
nikoam3 <amaya.niko@gmail.com>
2025-07-08 00:16:15 -0300

    first commit for request header parser

---
+++ Request Header Parser/index.js
@@ -0,0 +1,38 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip, // Dirección IP del cliente
+    language: req.get('Accept-Language')  || 'en-US', // Idioma del header Accept-Language
+    software: req.get('User-Agent'), // Software del header User-Agent
+    console: console.log(req.headers) // Imprime los headers en la consola
+  });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
